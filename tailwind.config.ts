0ddbdb0bac324a90a8bbafe8fc293760a61df406
Sandeeb Adhikari <sa2734@njit.edu>
2024-11-18 13:17:22 -0500

    tailwindcss.config.ts extend color and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import typography from '@tailwindcss/typography';
 
 export default {
   content: [
@@ -11,8 +12,47 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        peach: {
+          DEFAULT: '#E7816B',
+          rgb: 'rgb(231, 129, 107)',
+          hsl: 'hsl(11, 73%, 66%)'
+        },
+        black: {
+          DEFAULT: '#1D1C1E',
+          rgb: 'rgb(29, 28, 30)',
+          hsl: 'hsl(270, 3%, 11%)'
+        },
+        white: {
+          DEFAULT: '#FFFFFF',
+          rgb: 'rgb(255, 255, 255)',
+          hsl: 'hsl(0, 0%, 100%)'
+        },
+        'light-peach': {
+          DEFAULT: '#FFAD9B',
+          rgb: 'rgb(255, 173, 155)',
+          hsl: 'hsl(11, 100%, 80%)'
+        },
+        'dark-grey': {
+          DEFAULT: '#333136',
+          rgb: 'rgb(51, 49, 54)',
+          hsl: 'hsl(264, 5%, 20%)'
+        },
+        'light-grey': {
+          DEFAULT: '#F1F3F5',
+          rgb: 'rgb(241, 243, 245)',
+          hsl: 'hsl(210, 17%, 95%)'
+        }
       },
+      fontFamily: {
+        jost: ['Jost', 'sans-serif']
+      },
+      fontSize: {
+        h1: ['48px', { lineHeight: '48px', fontWeight: '500' }], // Jost Medium
+        h2: ['40px', { lineHeight: '48px', fontWeight: '500', letterSpacing: '0.125em' }],
+        h3: ['20px', { lineHeight: '26px', fontWeight: '500', letterSpacing: '0.3125em' }],
+        body: ['16px', { lineHeight: '26px', fontWeight: '400' }]
+      }
     },
   },
-  plugins: [],
+  plugins: [typography],
 } satisfies Config;
