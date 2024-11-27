868161b55d444ab440f8c6cf1bf2f07039f533b7
Sandeeb Adhikari <sa2734@njit.edu>
2024-11-26 19:48:51 -0500

    restored because it was not working reposiveness

--- components/ContactUsCard.tsx
+++ components/ContactUsCard.tsx
@@ -3,20 +3,20 @@ import ResponsiveImage from "./ResponsiveImage";
 
 const ContactUsCard = () => {
   return (
-    <div className="relative mx-[164px] h-[480px] bg-peach rounded-[15px] mt-[64px] overflow-hidden md:flex md:justify-between ">
+    <div className="relative mx-[164px] h-[480px] bg-peach rounded-[15px] mt-[64px] overflow-hidden">
       <div className="absolute -top-[160px] ">
         <ResponsiveImage folder="about" imageKey="hero" />
       </div>
-      <div className="absolute top-[135px] left-[95px] w-[540px] h-[210px] flex flex-col gap-8 lg:left-[150px]">
+      <div className="absolute top-[135px] left-[95px] w-[540px] h-[210px] flex flex-col gap-8">
         <h1 className="text-h1 text-white">Contact Us</h1>
-        <p className="text-body1 text-white w-[458px] h-[130px] sm:text-body">
+        <p className="text-body text-white w-[458px] h-[130px]">
           Ready to take it to the next level? Let’s talk about your project or
           idea and find out how we can help your business grow. If you are
           looking for unique digital experiences that’s relatable to your users,
           drop us a line.
         </p>
       </div>
-      <div className="absolute top-[55px] left-[635px] w-[380px] h-[371px] lg:left-[935px]">
+      <div className="absolute top-[55px] left-[635px] w-[380px] h-[371px] ">
         <form className="flex flex-col gap-6 ">
           <input
             type="text"
